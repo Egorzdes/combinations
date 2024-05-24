d210d36c404ba845f19a52eba9717cd88ed17bd0
fedoroveo1@mail.ru <fedorovEO27>
2024-05-24 14:03:03 +0300

    Add jenkins.groovy in project

--- jenkins/jenkins.groovy
+++ jenkins/jenkins.groovy
@@ -4,21 +4,18 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                // Шаг для получения исходного кода проекта из репозитория
                 git 'https://github.com/Egorzdes/combinations'
             }
         }
 
         stage('Build') {
             steps {
-                // Шаг для сборки проекта с использованием Maven
                 sh 'mvn clean package'
             }
         }
 
         stage('Deploy') {
             steps {
-                // Шаг для копирования собранного файла на локальную машину с использованием команды start /B
                 bat 'start /B cmd /c copy target\\BUILD.jar C:\\path\\to\\local\\deploy\\BUILD.jar'
             }
         }
