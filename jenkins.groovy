48daf64868e0d811a817a125df79890f99e4f77f
fedoroveo1@mail.ru <fedorovEO27>
2024-05-24 15:52:05 +0300

    Add jenkins.groovy in project

--- jenkins.groovy
+++ jenkins.groovy
@@ -2,6 +2,23 @@ pipeline {
     agent any
 
     stages {
+        stage('Preparation') {
+            steps {
+                script {
+                    // Проверяем, что Maven установлен
+                    def mvnHome = tool name: 'Maven 3.x', type: 'maven'
+                    echo "Maven tool home: ${mvnHome}"
+
+                    // Проверяем, что JDK установлен
+                    def jdkHome = tool name: 'JDK 1.8 (latest)', type: 'hudson.model.JDK'
+                    echo "JDK tool home: ${jdkHome}"
+
+                    // Обновляем пути для использования Maven и JDK
+                    env.PATH = "${mvnHome}/bin:${jdkHome}/bin:${env.PATH}"
+                }
+            }
+        }
+
         stage('Checkout') {
             steps {
                 checkout scm
@@ -11,7 +28,7 @@ pipeline {
         stage('Build') {
             steps {
                 script {
-                    // Предполагаем, что проект использует Maven для билда, можно заменить на нужный инструмент
+                    // Проверяем, что Maven проект действительно содержит pom.xml
                     if (fileExists('pom.xml')) {
                         sh 'mvn clean package'
                     } else {
@@ -24,16 +41,15 @@ pipeline {
         stage('Archive') {
             steps {
                 script {
-                    // Создаем архив c результатами билда
+                    // Создаем архив с результатами билда
                     def outputFile = 'build_archive.zip'
                     sh "zip -r ${outputFile} target/"
 
-                    // Артифакт, который будет доступен через интерфейс Jenkins
+                    // Артефакт, который будет доступен через интерфейс Jenkins
                     archiveArtifacts artifacts: outputFile
                 }
             }
         }
-
     }
 
     post {
