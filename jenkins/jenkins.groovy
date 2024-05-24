78e946d7e56826cd0b97990db8d049ad10dd9779
fedoroveo1@mail.ru <fedorovEO27>
2024-05-24 14:07:49 +0300

    Add jenkins.groovy in project

--- jenkins/jenkins.groovy
+++ jenkins/jenkins.groovy
@@ -4,13 +4,19 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git 'https://github.com/Egorzdes/combinations'
+                git branch: 'main', url: 'https://github.com/Egorzdes/combinations'
             }
         }
 
         stage('Build') {
             steps {
                 sh 'mvn clean package'
+            }
+        }
+
+        stage('Execute Jenkins Groovy Script') {
+            steps {
+                sh 'cat jenkins.groovy' // Здесь уточните путь к файлу jenkins.groovy в репозитории
             }
         }
     }
