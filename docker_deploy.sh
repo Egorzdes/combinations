53c1df0685639be1c3a111cb21eb2399c673209b
fedoroveo1@mail.ru <fedorovEO27>
2024-05-26 19:22:42 +0300

    1

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,7 +2,8 @@
 
 DOCKER_IMAGE_NAME="myapp"
 DOCKER_CONTAINER_NAME="myapp_container"
+DOCKER_HOST="tcp://localhost:2375"
 DIST_PATH="/var/jenkins_home/workspace/BUILD_JOB/"
 
-docker build -t $DOCKER_IMAGE_NAME $DIST_PATH
-docker run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+docker -H $DOCKER_HOST build -t $DOCKER_IMAGE_NAME $DIST_PATH
+docker -H $DOCKER_HOST run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
