a988d40bf50b7426c309e71ceee54d631e62e92e
fedoroveo1@mail.ru <fedorovEO27>
2024-05-26 16:00:56 +0300

    Add jenkins.groovy in project

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -5,10 +5,15 @@ DOCKER_CONTAINER_NAME="myapp_container"
 DIST_PATH="/var/jenkins_home/workspace/BUILD_JOB/"
 DOCKER_PATH="/usr/bin/docker"
 echo "Путь к Docker: $DOCKER_PATH"
-if [ ! -f "$DIST_PATH/Dockerfile" ]; then
-    echo "Dockerfile not found in $DIST_PATH"
-    exit 1
-fi
 sudo service docker start
 "$DOCKER_PATH" build -t $DOCKER_IMAGE_NAME $DIST_PATH
+if [ $? -ne 0 ]; then
+    echo "Failed to build the Docker image. Exiting."
+    exit 1
+fi
 "$DOCKER_PATH" run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+if [ $? -ne 0 ]; then
+    echo "Failed to run the Docker container. Exiting."
+    exit 1
+fi
+echo "Docker container $DOCKER_CONTAINER_NAME is running."
