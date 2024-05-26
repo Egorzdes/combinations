b42751109aa4e8a1c61653db0eabcd1d95eb89cb
fedoroveo1@mail.ru <fedorovEO27>
2024-05-26 16:16:18 +0300

    Add jenkins.groovy in project

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -2,21 +2,16 @@
 
 DOCKER_IMAGE_NAME="myapp"
 DOCKER_CONTAINER_NAME="myapp_container"
-DIST_PATH="/var/jenkins_home/workspace/BUILD_JOB/"
-DOCKER_PATH="/usr/bin/docker"
-echo "Путь к Docker: $DOCKER_PATH"
-cd $DIST_PATH
-"$DOCKER_PATH" build -t $DOCKER_IMAGE_NAME - <<EOF
-FROM scratch
-ADD . /
-EOF
+DOCKER_HOST="tcp://localhost:2375"
+docker -H $DOCKER_HOST build -t $DOCKER_IMAGE_NAME -f Dockerfile .
 
 if [ $? -ne 0 ]; then
     echo "Failed to build the Docker image. Exiting."
     exit 1
 fi
 
-"$DOCKER_PATH" run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+docker -H $DOCKER_HOST run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+
 if [ $? -ne 0 ]; then
     echo "Failed to run the Docker container. Exiting."
     exit 1
