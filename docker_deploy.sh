4daa5e685dc20613e973abe3b8dd9782b12e3d2f
fedoroveo1@mail.ru <fedorovEO27>
2024-05-26 13:30:44 +0300

    Jenkinsfile changes

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -3,9 +3,6 @@
 DOCKER_IMAGE_NAME="myapp"
 DOCKER_CONTAINER_NAME="myapp_container"
 
-DIST_PATH=$DIST_PATH_FROM_JOB
+
 
 docker build -t $DOCKER_IMAGE_NAME .
-
-docker run -d --name ${DOCKER_CONTAINER_NAME} -v ${DIST_PATH}:/app ${DOCKER_IMAGE_NAME}
-echo "Docker контейнер успешно создан и запущен для деплоя вашего дистрибутива на пути: $DIST_PATH"
