187ce37ad43bb78fb150407785f3d552e6bd4010
fedoroveo1@mail.ru <fedorovEO27>
2024-05-26 15:44:30 +0300

    Add jenkins.groovy in project

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -3,8 +3,8 @@
 DOCKER_IMAGE_NAME="myapp"
 DOCKER_CONTAINER_NAME="myapp_container"
 DIST_PATH="/var/jenkins_home/workspace/BUILD_JOB/"
-DOCKER_PATH="C:\Program Files\Docker\Docker\resources\bin"
+DOCKER_PATH="C:/Program Files/Docker/Docker/resources/bin/docker.exe"
 echo "Путь к Docker: $DOCKER_PATH"
-export PATH="$PATH:$DOCKER_PATH"
-docker build -t $DOCKER_IMAGE_NAME $DIST_PATH
-docker run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
+export PATH=$PATH:"$DOCKER_PATH"
+"$DOCKER_PATH" build -t $DOCKER_IMAGE_NAME $DIST_PATH
+"$DOCKER_PATH" run -d --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
