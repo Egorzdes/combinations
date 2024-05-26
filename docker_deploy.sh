9766bded64a3276ac0140a00eb423e9d1ebd3ad8
fedoroveo1@mail.ru <fedorovEO27>
2024-05-26 14:09:22 +0300

    Jenkinsfile changes

--- docker_deploy.sh
+++ docker_deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-
+set -x
 DOCKER_IMAGE_NAME="myapp"
 DOCKER_CONTAINER_NAME="myapp_container"
-DIST_PATH="/var/jenkins_home/workspace/BUILD_JOB/$1"
+DIST_PATH="/var/jenkins_home/workspace/BUILD_JOB/"
 
