160505f8b803f62b5fc6766702e89fd2c521fef9
Egor Fedorov <69429649+Egorzdes@users.noreply.github.com>
2024-12-02 02:52:28 +0300

    Delete docker_deploy.sh

--- docker_deploy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-DOCKER_IMAGE_NAME="proj"
-DOCKER_CONTAINER_NAME="myapp_container"
-DIST_PATH="/mnt/c/Users/79332/combinations/target"
-
-docker build -t $DOCKER_IMAGE_NAME -f $DIST_PATH/Dockerfile $DIST_PATH
-docker run -d --rm --name $DOCKER_CONTAINER_NAME $DOCKER_IMAGE_NAME
